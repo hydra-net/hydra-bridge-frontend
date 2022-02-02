297539536419719ce1a87c6c8f56eb197216e497
rimatik <slaven@rimatik.hr>
2022-02-02 12:04:57 +0100

    added getIsNotEnoughBalance to walletHelper.ts

--- src/helpers/walletHelper.ts
+++ src/helpers/walletHelper.ts
@@ -1,13 +1,41 @@
-import {
-    localStorageHelper,
-    LocalStorageKeys,
-  } from "./localStorageHelper";
-  
-  export const walletHelper = {
-    getLocalWallets: () => {
-      return localStorageHelper.get(LocalStorageKeys.wallets) || [];
-    },
-    setLocalWallets: (wallets: string[]) => {
-      return localStorageHelper.set(LocalStorageKeys.wallets, wallets);
-    },
-  };
+import { ethers } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
+import { TokenBalanceDto } from "../common/dtos";
+import { localStorageHelper, LocalStorageKeys } from "./localStorageHelper";
+
+export const walletHelper = {
+  getLocalWallets: () => {
+    return localStorageHelper.get(LocalStorageKeys.wallets) || [];
+  },
+  setLocalWallets: (wallets: string[]) => {
+    return localStorageHelper.set(LocalStorageKeys.wallets, wallets);
+  },
+};
+
+export const getIsNotEnoughBalance = (
+  walletBalances: TokenBalanceDto[],
+  value: number,
+  asset: number,
+  isWrongNetwork: boolean
+) => {
+  try {
+    const tokenBalanceDto = walletBalances?.find(
+      (tokenBalance) => tokenBalance.tokenId === asset
+    );
+    if (tokenBalanceDto && value && !isWrongNetwork) {
+      const parsedAmountToSpend = parseUnits(
+        value.toString(),
+        tokenBalanceDto.decimals
+      );
+      const amountInBig = ethers.BigNumber.from(parsedAmountToSpend);
+      const balanceBig = ethers.BigNumber.from(tokenBalanceDto.amount);
+
+      return amountInBig.gt(balanceBig);
+    }
+
+    return false;
+  } catch (e) {
+    console.log("Error checking wallet balance");
+    return false;
+  }
+};
