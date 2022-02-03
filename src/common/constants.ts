4ba5beab8bb6f7d105e967793cad0c49df6f6d47
rimatik <slaven@rimatik.hr>
2022-02-03 15:54:10 +0100

    added constants instead of strings

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,2 +1,19 @@
+import { SupportedChainId } from "./enums";
+
 export const ETH = "eth";
 export const USDC = "usdc";
+export const POLYGON_BRIDGE = "polygon-bridge";
+export const POLYGON_BRIDGE_GOERLI = "polygon-bridge-goerli";
+export const POLYGON = "polygon";
+export const HOP_BRIDGE = "hop-bridge";
+export const HOP_BRIDGE_GOERLI = "hop-bridge-goerli";
+export const HOP = "hop";
+
+export const NETWORK_EXPLORER_URLS: { [key in SupportedChainId]: string } = {
+  [SupportedChainId.MAINNET]: `https://etherscan.io/`,
+  [SupportedChainId.GOERLI]: `https://goerli.etherscan.io`,
+};
+
+export const ALL_SUPPORTED_CHAIN_IDS: SupportedChainId[] = Object.values(
+  SupportedChainId
+).filter((id) => typeof id === "number") as SupportedChainId[];
