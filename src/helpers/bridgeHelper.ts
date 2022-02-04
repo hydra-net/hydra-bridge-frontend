d84b8b43485ff6f33a3495bb08682ad63de8240e
rimatik <slaven@rimatik.hr>
2022-02-04 14:16:20 +0100

    added consts to getBridgeIconName instead of strings

--- src/helpers/bridgeHelper.ts
+++ src/helpers/bridgeHelper.ts
@@ -1,13 +1,19 @@
+import {
+  HOP,
+  HOP_BRIDGE,
+  HOP_BRIDGE_GOERLI,
+  POLYGON,
+  POLYGON_BRIDGE,
+  POLYGON_BRIDGE_GOERLI,
+} from "../common/constants";
+
 export const getBridgeIconName = (bridgeName: string): string => {
-  if (
-    bridgeName === "polygon-bridge" ||
-    bridgeName === "polygon-bridge-goerli"
-  ) {
-    return "polygon";
+  if (bridgeName === POLYGON_BRIDGE || bridgeName === POLYGON_BRIDGE_GOERLI) {
+    return POLYGON;
   }
 
-  if (bridgeName === "hop-bridge" || bridgeName === "hop-bridge-goerli") {
-    return "hop";
+  if (bridgeName === HOP_BRIDGE || bridgeName === HOP_BRIDGE_GOERLI) {
+    return HOP;
   }
 
   return "";
