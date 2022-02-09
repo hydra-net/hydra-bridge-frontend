165d49533198adbaaa37cc3227ccbc3e09de1ce4
rimatik <slaven@rimatik.hr>
2022-02-09 15:46:51 +0100

    added logic for filtering chains in case of testnet

--- src/common/hooks/useChains.ts
+++ src/common/hooks/useChains.ts
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import { getAllChains } from "../../api/commonService";
 import { ChainResponseDto } from "../dtos";
+import "dotenv/config";
+import { SupportedChainId } from "../enums";
+const { REACT_APP_DEFAULT_NETWORK_ID } = process.env;
 
 function useChains() {
   const [chains, setChains] = useState<ChainResponseDto[]>([]);
@@ -9,8 +12,13 @@ function useChains() {
     async function getChains() {
       try {
         const result = await getAllChains();
+
         if (result && result.length > 0) {
-          setChains(result);
+          const isTestnet =
+            parseInt(REACT_APP_DEFAULT_NETWORK_ID!) === SupportedChainId.GOERLI;
+          let filtered = result;
+          filtered = result.filter((chain) => chain.isTestnet === isTestnet);
+          setChains(filtered);
         }
       } catch (e) {
         console.log("Get chains failed:", e);
