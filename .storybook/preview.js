1c16b27f8feda3d4cce3de2798f4f4a1ed69fb44
Sadhill94 <sadhill.dev@gmail.com>
2022-02-09 23:56:45 +0100

    feat: setup storybook dependency and config

---
+++ .storybook/preview.js
@@ -0,0 +1,30 @@
+import { ThemeProvider } from "styled-components";
+import { addDecorator } from "@storybook/react";
+import { withThemes } from "@react-theming/storybook-addon";
+
+import { appViewports } from "./vieports.config";
+import { defaultTheme } from "../src/shell/theme/theme";
+
+import "./storybook.css";
+
+addDecorator(withThemes(ThemeProvider, [defaultTheme]));
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  backgrounds: {
+    disable: true,
+    grid: {
+      disable: true,
+    },
+  },
+  viewport: {
+    viewports: appViewports,
+    defaultViewport: "xl",
+  },
+};
---
+++ .storybook/vieports.config.js
@@ -0,0 +1,51 @@
+export const appViewports = {
+  xs: {
+    name: "xs",
+    styles: {
+      width: "375px",
+      height: "812px",
+    },
+  },
+  sm: {
+    name: "sm",
+    styles: {
+      width: "640px",
+      height: "801px",
+    },
+  },
+  md: {
+    name: "md",
+    styles: {
+      width: "768px",
+      height: "1024px",
+    },
+  },
+  lg: {
+    name: "lg",
+    styles: {
+      width: "1024px",
+      height: "1080px",
+    },
+  },
+  xl: {
+    name: "xl",
+    styles: {
+      width: "1280px",
+      height: "1080px",
+    },
+  },
+  "2xl": {
+    name: "2xl",
+    styles: {
+      width: "1536px",
+      height: "1080px",
+    },
+  },
+  "3xl": {
+    name: "3xl",
+    styles: {
+      width: "1920px",
+      height: "1080px",
+    },
+  },
+};
