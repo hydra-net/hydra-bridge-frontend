360a130aecd9c2aab423cfb52660dc26ea64f86b
Sadhill94 <sadhill.dev@gmail.com>
2022-02-11 03:52:56 +0100

    refactor: add missing decorator to inject style in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,11 +3,19 @@ import { addDecorator } from "@storybook/react";
 import { withThemes } from "@react-theming/storybook-addon";
 
 import { appViewports } from "./vieports.config";
-import { defaultTheme } from "../src/shell/theme/theme";
+import { stakenetTheme } from "../src/shell/theme/stakenetTheme";
+import { StakenetGlobalStyle } from "../src/shell/theme/stakenetGlobalStyle";
 
 import "./storybook.css";
+import "../src/assets/fonts/index.css";
 
-addDecorator(withThemes(ThemeProvider, [defaultTheme]));
+addDecorator(withThemes(ThemeProvider, [stakenetTheme]));
+addDecorator((s) => (
+  <>
+    <StakenetGlobalStyle />
+    {s()}
+  </>
+));
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
