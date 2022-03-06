25c5f9c9527921d7f96c646766a6b2f0ed513641
Sadhill94 <sadhill.dev@gmail.com>
2022-03-06 17:32:47 +0100

    refactor: import necessary assets

--- src/common/components/Icon/index.tsx
+++ src/common/components/Icon/index.tsx
@@ -1,5 +1,6 @@
 import { ReactComponent as hydraBridgeLogo } from "../../../assets/svg/hydra-bridge-logo.svg";
 import { ReactComponent as hydraBridgeLogoSm } from "../../../assets/svg/hydra-bridge-logo-sm.svg";
+import { ReactComponent as doubleArrowRight } from "../../../assets/svg/double-arrow-right.svg";
 import { ReactComponent as arrowRight } from "../../../assets/svg/arrow-right.svg";
 import { ReactComponent as cutArrowRight } from "../../../assets/svg/cut-arrow-right.svg";
 import { ReactComponent as ethereum } from "../../../assets/svg/ethereum.svg";
@@ -10,16 +11,23 @@ import { ReactComponent as copy } from "../../../assets/svg/copy-icon.svg";
 import { ReactComponent as arbitrum } from "../../../assets/svg/arbitrum.svg";
 import { ReactComponent as optimism } from "../../../assets/svg/optimism.svg";
 
+// coins
 import { ReactComponent as ethCoin } from "../../../assets/svg/coins/eth.svg";
 import { ReactComponent as daiCoin } from "../../../assets/svg/coins/dai.svg";
 import { ReactComponent as polygonCoin } from "../../../assets/svg/coins/polygon.svg";
 import { ReactComponent as usdcCoin } from "../../../assets/svg/coins/usdc.svg";
 import { ReactComponent as usdtCoin } from "../../../assets/svg/coins/usdt.svg";
 
+// networks
+import { ReactComponent as hopNetwork } from "../../../assets/svg/networks/hop.svg";
+import { ReactComponent as polygonNetwork } from "../../../assets/svg/networks/polygon.svg";
+import { ReactComponent as synapseNetwork } from "../../../assets/svg/networks/synapse.svg";
+
 export const icons = {
   hydraBridgeLogo,
   hydraBridgeLogoSm,
   arrowRight,
+  doubleArrowRight,
   cutArrowRight,
   ethereum,
   usdc,
@@ -34,6 +42,10 @@ export const icons = {
   polygonCoin,
   usdcCoin,
   usdtCoin,
+  // networks
+  hopNetwork,
+  polygonNetwork,
+  synapseNetwork,
 };
 
 export default icons;
