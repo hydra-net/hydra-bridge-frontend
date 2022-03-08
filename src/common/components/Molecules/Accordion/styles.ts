8d0a04164d6c1aca9e7b2ba55bf4fd2dff141a9d
Sadhill94 <sadhill.dev@gmail.com>
2022-03-08 09:10:18 +0100

    feat: create accordion component

---
+++ src/common/components/Molecules/Accordion/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const AccordionContainer = styled.div`
+  display: flex;
+  tab-index: 0;
+  flex-direction: column;
+  justify-content: center;
+  background-color: lightgrey;
+  border-radius: 10px;
+  height: auto;
+  padding: 2%;
+  width: 100%;
+  text-align: center;
+`;
+
+export const AccordionContent = styled.div`
+  width: 100%;
+`;
--- src/common/components/Molecules/BridgeRoutes/BridgeRoutes.tsx
+++ src/common/components/Molecules/BridgeRoutes/BridgeRoutes.tsx
@@ -1,11 +1,14 @@
 import RouteList from "./RouteList";
 import { RectangleSkeleton } from "../../Atoms/Skelletons/styles";
-import { Container, ContainerCard } from "../../Atoms/Containers/Container";
+import { Container } from "../../Atoms/Containers/Container";
 import { ContainerType } from "../../../enums";
 import { InputLabel as Label } from "../../Atoms/Label/Label";
 import { FlexWrapper } from "../../Atoms/Wrappers/Wrapper";
+import Accordion from "../Accordion/Accordion";
 
 import { RouteDto } from "../../../dtos";
+import RoundedBubble from "../../Atoms/RoundedBubble/RoundedBubble";
+import React, { ReactNode } from "react";
 type Props = {
   inProgress: boolean;
   routes: RouteDto[];
@@ -18,32 +21,35 @@ const BridgeRoutes = ({
   selectedRouteId,
   onRouteSelect,
 }: Props) => {
+  const renderHeader = (): ReactNode => (
+    <FlexWrapper
+      flexDirection={"row"}
+      alignItems={"center"}
+      justifyContent={"space-between"}
+    >
+      <Label margin={"0"}>Hello</Label>
+      <RoundedBubble>3</RoundedBubble>
+    </FlexWrapper>
+  );
+  const renderContent = (): ReactNode => {
+    return inProgress ? (
+      <>
+        <RectangleSkeleton height={"10rem"} style={{ marginBottom: "1rem" }} />
+        <RectangleSkeleton height={"10rem"} style={{ marginBottom: "1rem" }} />
+        <RectangleSkeleton height={"10rem"} />
+      </>
+    ) : (
+      <RouteList
+        routes={routes}
+        selectedRouteId={selectedRouteId!}
+        onRouteSelect={onRouteSelect}
+      />
+    );
+  };
+
   return (
     <Container type={ContainerType.XXXL} noGutter={true}>
-      <ContainerCard>
-        <FlexWrapper alignItems={"flex-start"}>
-          <Label>Available routes:</Label>
-        </FlexWrapper>
-        {inProgress ? (
-          <>
-            <RectangleSkeleton
-              height={"10rem"}
-              style={{ marginBottom: "1rem" }}
-            />
-            <RectangleSkeleton
-              height={"10rem"}
-              style={{ marginBottom: "1rem" }}
-            />
-            <RectangleSkeleton height={"10rem"} />
-          </>
-        ) : (
-          <RouteList
-            routes={routes}
-            selectedRouteId={selectedRouteId!}
-            onRouteSelect={onRouteSelect}
-          />
-        )}
-      </ContainerCard>
+      <Accordion header={renderHeader()} content={renderContent()} />
     </Container>
   );
 };
