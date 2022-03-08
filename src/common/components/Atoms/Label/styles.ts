fc574723b402a7b18e1e01679aed6ef019288771
Sadhill <sadhill.dev@gmail.com>
2022-03-08 13:24:10 +0100

    refactor: set font weight as semibold by default on labels (#37)

--- src/common/components/Atoms/Label/styles.ts
+++ src/common/components/Atoms/Label/styles.ts
@@ -12,7 +12,7 @@ export const StyledInputLabel = styled.label<StyledLabel>`
   font-size: ${theme.paragraph.md};
   color: ${(props) => (props.color ? props.color : theme.colors.white)};
   font-weight: ${(props) =>
-    props.fontWeight ? props.fontWeight : theme.fontWeight.normal};
+    props.fontWeight ? props.fontWeight : theme.fontWeight.semibold};
   margin: ${(props) =>
     props.margin ? props.margin : `0 0 ${theme.margin.md} 0`};
 
