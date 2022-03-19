cf3389690f05e636d6b288306472b2762411ba40
Sadhill94 <sadhill.dev@gmail.com>
2022-03-20 00:03:13 +0100

    fix: jest and reactI18n mock module broken after using i18n.t()

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,23 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+
+/**
+ * For useTranslation() we get the text by the json key
+ * For i18n.t() we get the text by the display value
+ */
+jest.mock("react-i18next", () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: (str: string) => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
+  initReactI18next: {
+    type: "3rdParty",
+    init: jest.fn(),
+  },
+}));
